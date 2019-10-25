9838e406dec70cb248ec6c368097567154e49fed
Dídac García <didaquis@gmail.com>
2019-10-25 11:17:16 +0200

    Add Lesson_16

---
+++ Lesson_16/src/functions/getGetTogethers.js
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk");
+const middy = require("middy");
+const { ssm } = require("middy/middlewares");
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const handler = async (event, context) => {
+	const count = 8;
+	const tableName = context.tableName;
+
+	const req = {
+		TableName: tableName,
+		Limit: count
+	};
+
+	const resp = await dynamodb.scan(req).promise();
+
+	const res = {
+		statusCode: 200,
+		body: JSON.stringify(resp.Items)
+	};
+
+	return res;
+};
+
+module.exports.handler = middy(handler).use(
+	ssm({
+		cache: true,
+		cacheExpiryInMillis: 3 * 60 * 1000,
+		setToContext: true,
+		names: {
+			tableName: `${process.env.getTogethersTableNamePath}`
+		}
+	})
+);
